e77b9ccb0bcd657846ecd3a182c4e84932b7d9a9
Rkyzzy <Rkyzzy@users.noreply.github.com>
2026-07-02 12:45:03 +0000

    deploy: 4e4053ad265f248ca419a6deef31a74292d5ba85

---
+++ _next/static/chunks/app/interests/page-2deaff529739101e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345,582],{3970:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[619,441,255,358],()=>e(e.s=3970)),_N_E=e.O()}]);
